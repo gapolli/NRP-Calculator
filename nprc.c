58f9a79008f72419524548a48c2ee9847d59fa5e
Gustavo Adrien Polli <gapolli@yahoo.com.br>
2020-04-02 18:48:32 -0300

    Initial code structure

---
+++ nprc.c
@@ -0,0 +1,39 @@
+/*
+ * Unicamp Faculty of Technology - FT
+ *
+ * SI010 - Data Structures II - 1st Half 2020
+ *
+ * Developers:
+ * 	Gustavo Adrien Polli	217357
+ * 	Victor Gomes Sampaio	225133
+ *
+ * Description:
+ * Non Regurar Purpose Calculator is a simple RPN (Reverse Polish
+ * Notation) write in C.
+ */
+
+/*
+ * Libraries
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Data structures
+ */
+// some struct here
+
+/*
+ * Function headers
+ */
+// some function headers here
+
+/*
+ * main function
+ */
+int main(int argc, char* argv[]){
+	return 0; //need to return system status
+}
+
+************************** FUNCTIONS SECTION **************************
+// some functions here
